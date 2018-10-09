ca9c5d1424af101bcdc8c5a21a912dcb73c91c6a
Dasol Park <dasol.park615@gmail.com>
2018-10-10 00:39:26 +0900

    MYSQL(SELECT, INSERT, UPDATE, DELETE) in JavaScript

--- database_mysql.js
+++ database_mysql.js
@@ -7,15 +7,47 @@ var conn = mysql.createConnection({
 });
 
 conn.connect();
-
+/*
 var sql = "SELECT * FROM topic";
 conn.query(sql, function(err, rows, fields){
 	if(err){
 		console.log(err);
 	}else{
-		console.log('rows', rows);
-		console.log('fields', fields);
+		for(var i=0; i<rows.length; i++){
+			console.log(rows[i].author);
+		}
 	}
 });
-
+*/
+/*
+const sql = 'INSERT INTO topic(title, description, author) VALUES(?, ?, ?)';
+const params = ['Supervisor', 'Watcher', 'graphitte'];
+conn.query(sql, params, function(err, rows, fields){
+	if(err){
+		console.log(err);
+	}else{
+		console.log(rows.insertId);
+	}
+});
+*/
+/*
+const sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+const params = ['NPM', 'leezche', '1', ];
+conn.query(sql, params, function(err, rows, fields){
+	if(err){
+		console.log(err);
+	}else{
+		console.log(rows);
+	}
+});
+*/
+const sql = 'DELETE FROM topic WHERE id=?';
+const params = [1];
+conn.query(sql, params, function(err, rows, fields){
+	if(err){
+		console.log(err);
+	}else{
+		console.log(rows);
+	}
+});
 conn.end();
