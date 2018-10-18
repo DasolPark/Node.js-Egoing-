1650e988125bfea62ba41b15a59c09eb5eb310cf
Dasol Park <dasol.park615@gmail.com>
2018-10-18 22:43:30 +0900

    Routes separate 3 (exports function)

--- app_routes.js
+++ app_routes.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-var p1 = require('./routes/p1');
-app.use('/p1', p1);///p1으로 들어오는 모든 요청은 라우터로 위임
+var p1 = require('./routes/p1')(app);//기존에 route만 반환하던 것이 이젠 함수가 됐음
+app.use('/p1', p1);
 
-var p2 = require('./routes/p2');
+var p2 = require('./routes/p2')(app);
 app.use('/p2', p2);
 
 app.listen(3003, function(req, res){
