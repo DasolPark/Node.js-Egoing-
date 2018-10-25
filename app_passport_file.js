c894840f1f268fb886fa4575ed82beab82cf095b
Dasol Park <dasol.park615@gmail.com>
2018-10-25 13:54:26 +0900

    add Federation Authentication(타사 인증) but not solved facebook login

--- app_passport_file.js
+++ app_passport_file.js
@@ -51,17 +51,18 @@ app.get('/welcome', function(req, res){
 });
 passport.serializeUser(function(user, done) {//딱 한 번 실행됨
 	console.log('serializeUser', user);
-  done(null, user.username);//해당 사용자를 구별할 수 있는 식별자를 두 번째 인자로 보냄
+  done(null, user.authId);//해당 사용자를 구별할 수 있는 식별자를 두 번째 인자로 보냄
 });//세션에 등록되고, 따라서 다음에 방문할 때도 이름을 기억함
 
 passport.deserializeUser(function(id, done) {//이미 등록되어있으면 이 func이 실행됨
 	console.log('deserializeUser', id);
 	for(var i=0; i<users.length; i++){
 		var user = users[i];
-		if(user.username === id){
+		if(user.authId === id){
 			return done(null, user);
 		}
 	}
+	done('There is no user.');
 });
 passport.use(new LocalStrategy(
 	function(username, password, done){//done은 함수를 담아주기로 약속되어 있음
@@ -85,14 +86,31 @@ passport.use(new LocalStrategy(
 ));
 passport.use(new FacebookStrategy({
     clientID: '277743539519773',
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://www.example.com/auth/facebook/callback"
+    clientSecret: '23228d7e4ecdba916cb22b1e891b6e71',
+    callbackURL: "/auth/facebook/callback",
+    profileFields: ['id', 'email', 'gender', 'link', 'locale', 
+    'name', 'timezone', 'updated_time', 'verified', 'displayName']
   },
   function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate(..., function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
+  	console.log(profile);//어떤 정보가 있는지 아는게 중요
+  	var authId = 'facebook: '+profile.id;
+  	for(var i=0; i<users.length; i++){//이미 사용자가 있는지 확인
+  		var user = users[i];
+  		if(user.authId === authId){//facebook 인증일 때는 authId가 있음
+  			return done(null, user);//user에는 사용자 정보가 들어있음
+  		}
+  	}
+  	var newuser = {//users에 사용자가 없을 때 push
+  		'authId': authId,//local의 username과 다르게 authId임
+  		'displayName': profile.displayName,
+  		'email': profile.emails[0].value
+  	};
+  	users.push(newuser);
+  	done(null, newuser);
+  //   User.findOrCreate(..., function(err, user) {
+  //     if (err) { return done(err); }
+  //     done(null, user);
+  //   });
   }
 ));
 app.post(
@@ -107,14 +125,25 @@ app.post(
   )
 );
 app.get(
-	'/auth/facebook', 
+	'/auth/facebook', //첫 번째 왕복
 	passport.authenticate(
-		'facebook'
+		'facebook',
+		{scope: 'email'}
 	)
+);//라우트가 2개임(타사 인증) 
+app.get('/auth/facebook/callback',//두 번째 왕복
+  passport.authenticate(
+  	'facebook', 
+  	{ 
+  		successRedirect: '/welcome',
+  	  failureRedirect: '/auth/login'
+  	}
+  )
 );
 var users = [//비밀번호 111111로 했음
 	{
 		//원래 id:1 이런식으로 들어가야하는데 여기서는 안 넣음 username으로도 가능하기 때문
+		authId: 'local:egoing',
 		username: 'egoing',
 		password: 'oGyGyN7iLuv4DqEvJ/kLMSS2M50ednsLA+MFrhGLYYBzyqY/LOEkcNRzU812pe6ht+3w7RkKp+vHAjQpeg6K3RVHDS2gHblB8X+F/i1xgSOcbe2xXUnqHnV+2JYEq1mhpS1iWDZwYIT1PYNjZeyYOJN4A6s6oTIXRTibNrVi4VQ=',
 		salt:'mvFaptKoXVqPxkwzMnlDF04PRfBY9rabSx2J/hGaWta98fK8CVDb2VKXodsiAKyFx6yDD1kM8rz7YTup0FGj1Q==',
@@ -124,6 +153,7 @@ var users = [//비밀번호 111111로 했음
 app.post('/auth/register', function(req, res){
 	hasher({password: req.body.password}, function(err, pass, salt, hash){
 		var user = {//k8805, 1234, K5
+			authId: 'local:'+req.body.username,//authId를 추가하여 local이든 facebook이든 authId로 식별
 			username: req.body.username,
 			password: hash,
 			salt: salt,
